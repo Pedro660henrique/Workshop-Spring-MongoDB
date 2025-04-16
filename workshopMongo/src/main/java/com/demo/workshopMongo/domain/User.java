4ced852190b5897fc1fe8b118632e2081f0ee8f0
Pedro660Henrique <pedro.santos660@hotmail.com>
2025-04-16 14:28:50 -0300

    Entity user and Rest running

---
+++ workshopMongo/src/main/java/com/demo/workshopMongo/domain/User.java
@@ -0,0 +1,19 @@
+package com.demo.workshopMongo.domain;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String nome;
+	private String email;
+
+}
