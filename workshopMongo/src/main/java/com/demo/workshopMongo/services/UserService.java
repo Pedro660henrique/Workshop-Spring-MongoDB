eaf39289647831991d3845fbed00f58599e894f8
Pedro660Henrique <pedro.santos660@hotmail.com>
2025-04-17 20:03:21 -0300

    user insertion with post

--- workshopMongo/src/main/java/com/demo/workshopMongo/services/UserService.java
+++ workshopMongo/src/main/java/com/demo/workshopMongo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.demo.workshopMongo.domain.User;
+import com.demo.workshopMongo.dto.UserDTO;
 import com.demo.workshopMongo.repository.UserRepository;
 import com.demo.workshopMongo.services.exception.ObjectNotFoundException;
 
@@ -21,5 +22,13 @@ public class UserService {
 	
 	public User findById(String id) {
 		 return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getNome(), objDTO.getEmail());
 	}
 }
